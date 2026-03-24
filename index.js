ae101f1b7668c827a6329c25d5ce194a5fb42cb7
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-03-24 15:12:03 +0600

    I don't know what I added

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ const express = require("express");
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-    res.send("Hello from expressjs!")
+    console.log("Here");
+    res.render("index")
 })
 
 app.listen(port, () => {
